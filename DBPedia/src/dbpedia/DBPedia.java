7e3f270d81120c0a42090f8d0c0d927348c0744a
Martin Leinberger <martin.leinberger@googlemail.com>
2012-08-16 20:04:20 +0200

    Created example query

--- DBPedia/src/dbpedia/DBPedia.java
+++ DBPedia/src/dbpedia/DBPedia.java
@@ -4,16 +4,22 @@
  */
 package dbpedia;
 
+import com.hp.hpl.jena.query.*;
+
 /**
  *
  * @author Martin
  */
 public class DBPedia {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String service = "http://dbpedia.org/sparql";
+        String queryString = "SELECT ?abstract WHERE { <http://dbpedia.org/resource/Java_(programming_language)> <http://dbpedia.org/ontology/abstract> ?abstract . FILTER (LANG(?abstract) = 'en') }";
+        Query query = QueryFactory.create(queryString);
+        QueryExecution execution = QueryExecutionFactory.sparqlService(service, query);
+        ResultSet result = execution.execSelect();
+        ResultSetFormatter.out(System.out, result, query);  
     }
 }
