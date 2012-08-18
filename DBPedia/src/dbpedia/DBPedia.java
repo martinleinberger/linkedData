39cca089efb85185d749a44d0c8122875cea7855
Martin Leinberger <martin.leinberger@googlemail.com>
2012-08-18 20:36:36 +0200

    updated DBPedia project to query all languages defined in the 101Wiki dump

--- DBPedia/src/dbpedia/DBPedia.java
+++ DBPedia/src/dbpedia/DBPedia.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  *
@@ -15,47 +16,59 @@ import java.io.IOException;
 public class DBPedia {
     private final static Gson GSON = new GsonBuilder().setPrettyPrinting().create();
     
+    private static JsonArray matchedObjects = new JsonArray();
+    private static JsonArray unmatchedObjects = new JsonArray();
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws FileNotFoundException, IOException {
         if (args.length < 2) {
-            System.out.println("Expected 2 parameters: wikiDump language [dbPedialanguage]");
+            System.out.println("Expected 2 parameters: wikiDump output");
             System.exit(1);
         }
         
         String wikiPath = args[0];
-        String language101 = args[1];
-        String dbPediaLanguage = language101;
-        
-        if (args.length > 2)
-            dbPediaLanguage = args[2];
-        
-        //Get data from wiki dump
-        JsonObject wikiObj = new JsonParser().parse(new JsonReader(new FileReader(wikiPath))).getAsJsonObject();
-        JsonObject languages = wikiObj.get("Language").getAsJsonObject();
-        JsonObject specificLanguage = languages.get(language101).getAsJsonObject();
+        String output = args[1];
         
         //prepare query
         String service = "http://dbpedia.org/sparql";
         String queryString = "SELECT ?abstract WHERE { <http://dbpedia.org/resource/:language:> <http://dbpedia.org/ontology/abstract> ?abstract . FILTER (LANG(?abstract) = 'en') }";
-        String finalQuery = queryString.replace(":language:", dbPediaLanguage);
         
-        //create and execute query
-        Query query = QueryFactory.create(finalQuery);
-        QueryExecution execution = QueryExecutionFactory.sparqlService(service, query);
-        ResultSet result = execution.execSelect();
+        //Get data from wiki dump
+        JsonObject wikiObj = new JsonParser().parse(new JsonReader(new FileReader(wikiPath))).getAsJsonObject();
+        JsonObject languages = wikiObj.get("Language").getAsJsonObject();
+        
+        //iterate through all languages
+        for (Map.Entry<String, JsonElement> entry : languages.entrySet()) {
+            //create and execute final query
+            String finalQuery = queryString.replace(":language:", entry.getKey());
+            Query query = QueryFactory.create(finalQuery);
+            QueryExecution execution = QueryExecutionFactory.sparqlService(service, query);
+            ResultSet result = execution.execSelect();
+            
+            //process the results
+            if (result.hasNext()) {
+                //an entry was found => combine the results
+                JsonObject combinedResult = new JsonObject();
+                combinedResult.add("name", entry.getValue().getAsJsonObject().get("name"));
+                combinedResult.add("url", entry.getValue().getAsJsonObject().get("url"));
+                combinedResult.add("implementations", entry.getValue().getAsJsonObject().get("implementations"));
+                combinedResult.add("abstract", new JsonPrimitive(result.next().get("abstract").toString()));
                 
-        //combine the results
-        JsonObject combinedResult = new JsonObject();
-        combinedResult.add("name", specificLanguage.get("name"));
-        combinedResult.add("url", specificLanguage.get("url"));
-        combinedResult.add("implementations", specificLanguage.get("implementations"));
-        combinedResult.add("abstract", new JsonPrimitive(result.next().get("abstract").toString()));
+                matchedObjects.add(combinedResult);
+            } else //nothing found => just add the data from the 101Wiki dump
+                unmatchedObjects.add(entry.getValue());
+        }
         
-        //write output
-       FileWriter writer = new FileWriter("output.json");
-       writer.write(GSON.toJson(combinedResult));
-       writer.close();
+        
+        //create and write the result
+        JsonObject result = new JsonObject();
+        result.add("matched", matchedObjects);
+        result.add("unmatched", unmatchedObjects);
+        
+        FileWriter writer = new FileWriter(output);
+        writer.write(GSON.toJson(result));
+        writer.close();
     }
 }
